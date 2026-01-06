2d8023343f56302884b53b28e4f0c2514470d189
Khaled Hasan Rony <khaledhasanrony@gmail.com>
2026-01-07 05:20:30 +0600

    updated

--- routes/console.php
+++ routes/console.php
@@ -6,8 +6,10 @@ use App\Jobs\DailySalesReport;
 app()->booted(function () {
     $schedule = app(Schedule::class);
 
-    // schedule daily sales report
-    // runs daily at 6 PM
+    /**
+     * schedule daily sales report
+     * runs daily at 6 PM
+     */
 //    $schedule->job(new DailySalesReport)->dailyAt('18:00');
 
     $schedule->job(new DailySalesReport)->everyMinute();
